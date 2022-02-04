14cbec8ecff0bb4da7dac4cf6ea7bf482a297ab5
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-04 22:41:43 +0300

    generate components,classes,and directive

--- Quotes-App/src/app/app.module.ts
+++ Quotes-App/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteHighlightDirective } from './quote-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    QuoteComponent,
+    DateCountPipe,
+    QuoteHighlightDirective
   ],
   imports: [
     BrowserModule,
